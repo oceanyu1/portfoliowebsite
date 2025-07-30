7efde7431183635bbbab2312d3705ec59b7fc36f
oceanyu1 <81434548+oceanyu1@users.noreply.github.com>
2025-07-30 18:39:18 -0400

    Complete navbar so it redirects to 5 different pages: About, Skills, Projects, Resume, Contact

--- __init__.py
+++ __init__.py
@@ -3,8 +3,24 @@ from flask import Flask, redirect, url_for, render_template, request, session, f
 app = Flask(__name__)
 
 @app.route("/")
-def aboutme():
+def about():
     return render_template("index.html")
+
+@app.route("/skills")
+def skills():
+    return render_template("skills.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/resume")
+def resume():
+    return render_template("resume.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
