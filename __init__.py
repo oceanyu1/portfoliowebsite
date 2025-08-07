bb959203be6171642d53e8be2f90d6729d4e635c
oceanyu1 <81434548+oceanyu1@users.noreply.github.com>
2025-08-06 22:36:06 -0400

    Prepare for deployment

--- __init__.py
+++ __init__.py
@@ -1,7 +1,8 @@
 from flask import Flask, redirect, url_for, render_template, request, session, flash
+import os 
 
 app = Flask(__name__)
-app.secret_key = 'some_secret_key'
+app.secret_key = os.environ.get('SECRET_KEY', 'dev-key-for-local-testing-only')
 
 @app.route("/")
 def about():
@@ -20,4 +21,4 @@ def thank_you():
     return render_template('thankyou.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
